17a52299cf1e62a118bed2287de47b2d63359b10
helbing23 <helbinrapheal23@gmai.com>
2019-09-24 10:33:29 +0530

    Updates

---
+++ precache-manifest.057fb74495fd8f87467fdb4cee507447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f8dbc2ce06dd73030ce4dda80d329e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0729b041fd504c46387b",
+    "url": "/static/css/main.c90e83f6.chunk.css"
+  },
+  {
+    "revision": "d43bb0229429cc611f6e",
+    "url": "/static/js/2.d61e513d.chunk.js"
+  },
+  {
+    "revision": "0729b041fd504c46387b",
+    "url": "/static/js/main.e251a74e.chunk.js"
+  },
+  {
+    "revision": "36cf11b5c2ba8a5d6fb1",
+    "url": "/static/js/runtime~main.0905e18a.js"
+  }
+]);
