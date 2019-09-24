741d7d6a9402a033f29613462ff8d6eb098dd836
helbing23 <helbinrapheal23@gmai.com>
2019-09-24 10:42:42 +0530

    Updates

---
+++ precache-manifest.00270daec0d11620efe4561a3a34de90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b44c3d0b33eb0e701ab7e14a90b84ca",
+    "url": "/react-todolistapp/index.html"
+  },
+  {
+    "revision": "eb8323ddb71d66bd52de",
+    "url": "/react-todolistapp/static/css/main.c90e83f6.chunk.css"
+  },
+  {
+    "revision": "d43bb0229429cc611f6e",
+    "url": "/react-todolistapp/static/js/2.d61e513d.chunk.js"
+  },
+  {
+    "revision": "eb8323ddb71d66bd52de",
+    "url": "/react-todolistapp/static/js/main.392fb2be.chunk.js"
+  },
+  {
+    "revision": "263b5a917aaf039f0546",
+    "url": "/react-todolistapp/static/js/runtime~main.1491eafe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.057fb74495fd8f87467fdb4cee507447.js"
+  "/react-todolistapp/precache-manifest.00270daec0d11620efe4561a3a34de90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todolistapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
